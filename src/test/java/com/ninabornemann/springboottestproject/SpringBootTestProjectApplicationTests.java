3f9702edc3a8b89854ae5d81f7b9a6cc64870f94
Nina Bornemann <nina.bornemann@freenet.de>
2025-09-12 01:55:40 -0700

    Initial commit

---
+++ src/test/java/com/ninabornemann/springboottestproject/SpringBootTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninabornemann.springboottestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
